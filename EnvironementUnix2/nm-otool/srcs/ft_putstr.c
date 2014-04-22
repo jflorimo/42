ff8e0798a2e5bf885cddc519a2ddd2ed1c67074a
Jean-Christophe FLORIMONT <jflorimo@e2r4p17.42.fr>
2014-04-22 18:30:53 +0200

    push nm-otool

---
+++ EnvironementUnix2/nm-otool/srcs/ft_putstr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jflorimo <jflorimo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/04/20 11:58:39 by jflorimo          #+#    #+#             */
+/*   Updated: 2014/04/20 11:58:41 by jflorimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "nm.h"
+
+void	ft_putstr(char const *s)
+{
+	if (s)
+	{
+		while (*s)
+			write(1, s++, 1);
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr_base(size_t n, size_t base)
+{
+	if (n >= base)
+	{
+		ft_putnbr_base(n / base, base);
+		ft_putnbr_base(n % base, base);
+	}
+	else
+	{
+		if (n < 10)
+			ft_putchar(n + '0');
+		else
+			ft_putchar(n - 10 + 'A');
+	}
+}
+
+void	ft_putmem(size_t n)
+{
+	ft_putstr("0x");
+	ft_putnbr_base(n, 16);
+}
