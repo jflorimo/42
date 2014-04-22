24f5115cfda0cb70d27f6a4f2eb6dd9a39b0f2f9
Jean-Christophe FLORIMONT <jflorimo@e2r4p17.42.fr>
2014-04-22 19:01:53 +0200

    main

--- EnvironementUnix2/nm-otool/srcs/main.c
+++ EnvironementUnix2/nm-otool/srcs/main.c
@@ -9,13 +9,30 @@
 /*   Updated: 2014/04/22 16:41:37 by jflorimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "nm.h"
 #include "libft.h"
 
-int 	main(int ac, char **av)
+void		ft_read_file(char *s)
 {
-	int	i;
+	int		fd;
+	char	*line;
+
+	fd = open(s, O_RDONLY);
+	line = NULL;
+	while(get_next_line(fd, &line))
+	{
+		ft_putstr(line);
+		ft_putstr("\n");
+		free(line);
+	}
+}
+
+int 		main(int ac, char **av)
+{
+	int		i;
 
 	i = 1;
 	if (ac > 1)
@@ -24,7 +41,7 @@ int 	main(int ac, char **av)
 		{
 			ft_putstr(av[i]);
 			ft_putstr("\n");
-			// ft_read_file(av[i]);
+			ft_read_file(av[i]);
 			i++;
 		}
 	}
