45fc0b8200ce5acda31989f4692f7f2ea3df8743
Jean <jflorimo@student.42.fr>
2014-05-17 15:36:46 +0200

    addind colors for write

--- base/clean_project_base/includes/script.h
+++ base/clean_project_base/includes/script.h
@@ -13,4 +13,22 @@
 #ifndef SCRIPT_H
 # define SCRIPT_H
 
+# define ANSI_COLOR_BLACK                "\x1b[30m"
+# define ANSI_COLOR_RED                    "\x1b[31m"
+# define ANSI_COLOR_GREEN                "\x1b[32m"
+# define ANSI_COLOR_YELLOW                "\x1b[33m"
+# define ANSI_COLOR_BLUE                "\x1b[34m"
+# define ANSI_COLOR_MAGENTA                "\x1b[35m"
+# define ANSI_COLOR_CYAN                "\x1b[36m"
+# define ANSI_COLOR_WHITE                "\x1b[36m"
+# define ANSI_BACKGROUND_COLOR_BLACK    "\x1b[40m"
+# define ANSI_BACKGROUND_COLOR_RED        "\x1b[41m"
+# define ANSI_BACKGROUND_COLOR_GREEN    "\x1b[42m"
+# define ANSI_BACKGROUND_COLOR_YELLOW    "\x1b[43m"
+# define ANSI_BACKGROUND_COLOR_BLUE        "\x1b[44m"
+# define ANSI_BACKGROUND_COLOR_MAGENTA    "\x1b[45m"
+# define ANSI_BACKGROUND_COLOR_CYAN        "\x1b[46m"
+# define ANSI_BACKGROUND_COLOR_WITHE    "\x1b[47m"
+# define ANSI_COLOR_RESET                "\x1b[0m"
+
 #endif
